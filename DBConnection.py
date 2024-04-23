cc756f07290c968a1ddda81946e20eaa10375473
Sharika Khondaker <sharika.khondaker@gmail.com>
2024-04-23 16:17:41 -0400

    made a few changes, fixed the nav bar

--- DBConnection.py
+++ DBConnection.py
@@ -1,4 +1,4 @@
-#import mysql.connector
+import mysql.connector
 import hashlib
 from flask import Flask, Blueprint, request, jsonify, render_template
 
@@ -14,23 +14,69 @@ def create_account():
   user = passedInfo['username']
   pw = passedInfo['password']
 
-  #connect to database
-  #conn = mysql.connector.connect(host='localhost', password='rootSQL', user='root', database='minimentordb')
-  #if conn.is_connected():
-  #  print("Connection established...")
-  #cur = conn.cursor()
-
-  #TODO check if username is in database
   #TODO check for number, capital letter, >=5 characters in pw
 
-  #pw = hashlib.sha256(pw.encode()).hexdigest()  #encode password
+  #connect to database
+  conn = mysql.connector.connect(host='localhost', password='rootSQL', user='root', database='minimentordb')
+  if conn.is_connected():
+    print("Connection established...")
+  cur = conn.cursor()
 
-  #insert_stmt = (
-  #  "INSERT INTO userdata4 (username, password) "
-  #  "VALUES (%s, %s)"
-  #)
-  #cur.execute(insert_stmt, (user, pw))
+  #check if username is in database
+  search_stmt = (
+    "SELECT * FROM userdata4 WHERE username = %s"
+  )
+  cur.execute(search_stmt, (user, ))
+  entries = cur.fetchall();
+  
+  if len(entries) > 0:
+    return jsonify({'username': "ERROR CODE", 'password': "Username taken, please try again"})
+  
+  insert_stmt = (
+   "INSERT INTO userdata4 (username, password) "
+   "VALUES (%s, %s)"
+  )
+  cur.execute(insert_stmt, (user, pw))
 
-  #conn.commit()
+  conn.commit()
+  conn.close()
+  return jsonify({'username': user, 'password': pw})
+
+@database_bp.route('/acct-login', methods=['POST'])
+def acct_login():
+  #check if account is in database
+  passedInfo = request.json.get('content')
+  user = passedInfo['username']
+  pw = passedInfo['password']
+
+  #connect to database
+  conn = mysql.connector.connect(host='localhost', password='rootSQL', user='root', database='minimentordb')
+  if conn.is_connected():
+    print("Connection established...")
+  cur = conn.cursor()
+
+  #check if username is in database
+  search_stmt = (
+    "SELECT * FROM userdata4 WHERE username = %s"
+  )
+  cur.execute(search_stmt, (user, ))
+  entries = cur.fetchall();
+  
+  if len(entries) == 0:
+    return jsonify({'username': "ERROR CODE", 'password': "Username doesn't exist, please create account or try again"})
+
+  #check if password matches
+  search_stmt2 = (
+    "SELECT * FROM userdata4 WHERE username = %s AND password = %s"
+  )
+  cur.execute(search_stmt2, (user, pw))
+  entries2 = cur.fetchall();
+
+  if len(entries2) == 0:
+    return jsonify({'username': "ERROR CODE", 'password': "Wrong password, please try again"})
+  
+  conn.commit()
+  conn.close()
 
   return jsonify({'username': user, 'password': pw})
+  
